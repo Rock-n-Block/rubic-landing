c1c2455d0a8f60ef984b95d988669de8210fb6c8
Dmitry Kovalev <kovalyov1987@gmail.com>
2020-10-23 14:49:49 +0300

    Added language switcher

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -7,6 +7,8 @@ import {
   EventEmitter,
 } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: 'app-header-main-page',
@@ -20,13 +22,40 @@ export class HeaderMainPageComponent implements OnInit {
   public infoMenuOpened;
   public devMenuOpened;
   public productMenuOpened;
+  public openedLngList: boolean;
+  private translator: TranslateService;
+  public languagesList: { lng: string; title: string; active?: boolean }[];
+  public currLanguage: string;
 
   @ViewChild('logoutConfirmation') logoutConfirmation: TemplateRef<any>;
   @ViewChild('headerPage') headerPage;
 
   @Output() changedSocialState = new EventEmitter<string>();
 
-  constructor(private router: Router) {
+  constructor(
+      private router: Router,
+      private translate: TranslateService,
+      private cookieService: CookieService
+  ) {
+    this.translator = translate;
+    this.languagesList = [
+      {
+        lng: "en",
+        title: "English",
+      },
+      {
+        lng: "ko",
+        title: "한국어",
+      },
+    ];
+
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.setActiveLanguage(event);
+    });
+    this.setActiveLanguage({
+      lang: translate.currentLang,
+    });
+
     document
       .getElementsByTagName('body')[0]
       ['addEventListener']('mousedown', () => {
@@ -45,6 +74,32 @@ export class HeaderMainPageComponent implements OnInit {
       }
     });
   }
+
+  private setActiveLanguage(event) {
+    if (this.currLanguage) {
+      this.languagesList.filter((lang) => {
+        return lang["lng"] === this.currLanguage;
+      })[0].active = false;
+    }
+    this.currLanguage = event.lang;
+    this.cookieService.set("lng", this.currLanguage);
+
+    this.languagesList.filter((lang) => {
+      return lang["lng"] === this.currLanguage;
+    })[0].active = true;
+    this.languagesList.sort((a, b) => {
+      return b.active ? 1 : -1;
+    });
+  }
+
+  public toggleLanguage() {
+    this.openedLngList = !this.openedLngList;
+  }
+
+  public setLanguage(lng) {
+    this.translator.use(lng);
+  }
+
 
   ngOnInit() {}
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements OnInit {
-  @ViewChild('listingModal') listing: TemplateRef<any>;
-
-  protected listingModal: MatDialogRef<any>;
-
-  constructor(protected route: ActivatedRoute, private dialog: MatDialog) {}
-
-  ngOnInit() {
-    // this.openModal();
-  }
-
-  public openModal() {
-    this.listingModal = this.dialog.open(this.listing, {
-      width: '500px',
-      panelClass: 'dialog-listing-container',
-    });
-  }
+export class MainPageComponent {
+  constructor() {}
 }
