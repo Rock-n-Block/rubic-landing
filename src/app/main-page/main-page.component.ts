d24c962b41deb5bc63f71b6f23d36517e8f9e516
Dmitry Kovalev <kovalyov1987@gmail.com>
2020-10-26 19:35:59 +0300

    Fixes korean language

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
-  constructor() {}
+  public lng: string;
+  public onePagerLinks = {
+    ko: 'https://drive.google.com/file/d/1ve7XKm7gbK3AuyniNX2TO2JCR8fuXAbe/',
+    en: 'https://drive.google.com/file/d/1Ab3DmoM02689MJoItdXlywvTvdtihp5_/view'
+  };
+  constructor(
+      private translate: TranslateService,
+      ) {
+
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lng = event.lang;
+    });
+    this.lng = translate.currentLang;
+  }
 }
